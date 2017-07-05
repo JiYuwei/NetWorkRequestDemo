a1527dab14e13b6ba267b4f6a868f456bc20f8d6
纪宇伟 <jiyuwei@jiyuweideMacBook-Pro.local>
2017-07-05 16:10:15 +0800

    ready

---
+++ NetWorkRequestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetWorkRequestDemo
+//
+//  Created by 纪宇伟 on 2017/7/5.
+//  Copyright © 2017年 jyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NetWorkRequestDemo/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  NetWorkRequestDemo
+//
+//  Created by 纪宇伟 on 2017/7/5.
+//  Copyright © 2017年 jyw. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.navigationItem.title = @"AFNetworkingTest";
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

11d8a3ab1c93e61b7c3be278ff9dee1393789fa9
纪宇伟 <jiyuwei@jiyuweideMacBook-Pro.local>
2017-07-05 16:13:43 +0800

    ready

---
+++ NetWorkRequestDemo/JYNetworkRequest/JYNetworkRequest.h
@@ -0,0 +1,13 @@
+//
+//  JYNetworkRequest.h
+//  NetWorkRequestDemo
+//
+//  Created by 纪宇伟 on 2017/7/5.
+//  Copyright © 2017年 jyw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JYNetworkRequest : NSObject
+
+@end
---
+++ NetWorkRequestDemo/JYNetworkRequest/JYNetworkRequest.m
@@ -0,0 +1,13 @@
+//
+//  JYNetworkRequest.m
+//  NetWorkRequestDemo
+//
+//  Created by 纪宇伟 on 2017/7/5.
+//  Copyright © 2017年 jyw. All rights reserved.
+//
+
+#import "JYNetworkRequest.h"
+
+@implementation JYNetworkRequest
+
+@end
